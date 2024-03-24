8b9a96309e6dac7673c0b7bf589e001cebdf8ee3
Ayush Bhardwaj <40769053+ayushbhardwaj10@users.noreply.github.com>
2024-03-23 20:17:41 -0700

    dockerized the application.
    - Created the images separately for React, Flask and utilized sql ready to image.

--- backend/utils/dbConnection.py
+++ backend/utils/dbConnection.py
@@ -1,15 +1,23 @@
 import pymysql.cursors
 
 # Database connection parameters
+# db_config = {
+#     "host": "localhost",
+#     "user": "root",
+#     "password": "mickey123",
+#     "db": "energy",
+#     "charset": 'utf8mb4',
+#     "cursorclass": pymysql.cursors.DictCursor
+# }
+
 db_config = {
-    "host": "localhost",
-    "user": "root",
-    "password": "mickey123",
-    "db": "energy",
+    "host": "db",  # Use the service name defined in docker-compose.yml
+    "user": "user",  # The MYSQL_USER you defined in docker-compose.yml
+    "password": "password",  # The MYSQL_PASSWORD you defined in docker-compose.yml
+    "db": "energy",  # The MYSQL_DATABASE you defined in docker-compose.yml
     "charset": 'utf8mb4',
     "cursorclass": pymysql.cursors.DictCursor
 }
-
 
 
 
--- frontend/energyapp/src/utils/Constants.js
+++ frontend/energyapp/src/utils/Constants.js
@@ -1,8 +1,8 @@
 export const MODE = 0; // set 0 for development and 1 for production
-export const SIGNUP_API_URL = ["http://127.0.0.1:5000/signup", "replace with production api in future"];
-export const SIGNIN_API_URL = ["http://127.0.0.1:5000/login", ""];
-export const TOKENVALID_API_URL = ["http://127.0.0.1:5000/tokenValid", ""];
-export const LINEGRAPH_DATA_API_URL = ["http://127.0.0.1:5000/linegraph", ""];
-export const PIECHART_DATA_API_URL = ["http://127.0.0.1:5000/pieChartData", ""];
-export const PAIRWISE_BAR_GRAPH_DATA_API_URL = ["http://127.0.0.1:5000/getEnergyDateWise", ""];
-export const GLOBAL_ENERGY_DATA_API_URL = ["http://127.0.0.1:5000/GlobalEnergyData", ""];
+export const SIGNUP_API_URL = ["http://127.0.0.1:5001/signup", "replace with production api in future"];
+export const SIGNIN_API_URL = ["http://127.0.0.1:5001/login", ""];
+export const TOKENVALID_API_URL = ["http://127.0.0.1:5001/tokenValid", ""];
+export const LINEGRAPH_DATA_API_URL = ["http://127.0.0.1:5001/linegraph", ""];
+export const PIECHART_DATA_API_URL = ["http://127.0.0.1:5001/pieChartData", ""];
+export const PAIRWISE_BAR_GRAPH_DATA_API_URL = ["http://127.0.0.1:5001/getEnergyDateWise", ""];
+export const GLOBAL_ENERGY_DATA_API_URL = ["http://127.0.0.1:5001/GlobalEnergyData", ""];
